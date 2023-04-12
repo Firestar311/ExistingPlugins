ebb5f88e9297d5bcca7baaf4602e8bf858638e9b
Firestar311 <slaterandrew0398@gmail.com>
2023-04-12 05:24:04 -0500

    Delete Old FirecraftMC/StarLib/src/main/java/com/starmediadev/lib/oldsql directory

--- Old FirecraftMC/StarLib/src/main/java/com/starmediadev/lib/oldsql/TableInfo.java
+++
@@ -1,9 +0,0 @@
-package com.starmediadev.lib.oldsql;
-
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
-public @interface TableInfo {
-    String name();
-}
