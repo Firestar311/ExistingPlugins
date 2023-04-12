75ba3b4371d0038386fa118ec9829d3bc973b940
Firestar311 <slaterandrew0398@gmail.com>
2023-04-12 05:23:44 -0500

    Delete Old FirecraftMC/StarLib/src/main/java/com/starmediadev/lib/gui directory

--- Old FirecraftMC/StarLib/src/main/java/com/starmediadev/lib/gui/ButtonListener.java
+++
@@ -1,9 +0,0 @@
-package com.starmediadev.lib.gui;
-
-import org.bukkit.event.inventory.InventoryClickEvent;
-
-public interface ButtonListener {
-    
-    void onClick(InventoryClickEvent event);
-    
-}
