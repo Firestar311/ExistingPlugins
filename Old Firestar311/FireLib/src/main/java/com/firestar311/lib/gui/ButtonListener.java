c190bf2ecdec20b0e567d2e8685852f8c5751107
Firestar311 <slaterandrew0398@gmail.com>
2023-06-25 06:39:07 -0500

    Delete Old Firestar311/FireLib/src/main/java/com/firestar311/lib/gui directory

--- Old Firestar311/FireLib/src/main/java/com/firestar311/lib/gui/ButtonListener.java
+++
@@ -1,9 +0,0 @@
-package com.firestar311.lib.gui;
-
-import org.bukkit.event.inventory.InventoryClickEvent;
-
-public interface ButtonListener {
-    
-    void onClick(InventoryClickEvent event);
-    
-}
