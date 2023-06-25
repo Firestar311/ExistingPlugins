05f91b787cf81e6d7ef44f730126b06bcbfff8b1
Firestar311 <slaterandrew0398@gmail.com>
2023-06-25 06:43:12 -0500

    Delete Old FirecraftMC/FirecraftAPI/src/java/net/firecraftmc/api/exceptions directory

--- Old FirecraftMC/FirecraftAPI/src/java/net/firecraftmc/api/exceptions/CornersNotInSameWorldException.java
+++
@@ -1,7 +0,0 @@
-package net.firecraftmc.api.exceptions;
-
-public class CornersNotInSameWorldException extends Exception {
-    public CornersNotInSameWorldException() {
-        super("Corners are not in the same world.");
-    }
-}
