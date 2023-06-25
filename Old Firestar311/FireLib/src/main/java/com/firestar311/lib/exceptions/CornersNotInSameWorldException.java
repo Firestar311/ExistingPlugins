ddb2a330b10b0c7c4df49342944c344f640312f5
Firestar311 <slaterandrew0398@gmail.com>
2023-06-25 06:38:56 -0500

    Delete Old Firestar311/FireLib/src/main/java/com/firestar311/lib/exceptions directory

--- Old Firestar311/FireLib/src/main/java/com/firestar311/lib/exceptions/CornersNotInSameWorldException.java
+++
@@ -1,7 +0,0 @@
-package com.firestar311.lib.exceptions;
-
-public class CornersNotInSameWorldException extends RuntimeException {
-    public CornersNotInSameWorldException() {
-        super("Corners are not in the same world.");
-    }
-}
