22b88736e31bef99b1df6e3c1d1ccb822c472f86
Firestar311 <slaterandrew0398@gmail.com>
2023-06-25 06:39:30 -0500

    Delete Old Firestar311/FireLib/src/main/java/com/firestar311/lib/sql directory

--- Old Firestar311/FireLib/src/main/java/com/firestar311/lib/sql/IRecord.java
+++
@@ -1,6 +0,0 @@
-package com.firestar311.lib.sql;
-
-public interface IRecord<T> {
-    
-    String replaceStatementValues(String statement);
-}
--- Old Firestar311/FireLib/src/main/java/com/firestar311/lib/sql/TableInfo.java
+++
@@ -1,9 +0,0 @@
-package com.firestar311.lib.sql;
-
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
-public @interface TableInfo {
-    String name();
-}
