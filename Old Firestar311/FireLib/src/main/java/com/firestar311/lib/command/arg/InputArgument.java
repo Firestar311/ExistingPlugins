baa0ce776569b8a044d746f428ed2f58ffa1aa6f
Firestar311 <slaterandrew0398@gmail.com>
2023-06-25 06:38:38 -0500

    Delete Old Firestar311/FireLib/src/main/java/com/firestar311/lib/command directory

--- Old Firestar311/FireLib/src/main/java/com/firestar311/lib/command/arg/InputArgument.java
+++
@@ -1,6 +0,0 @@
-package com.firestar311.lib.command.arg;
-
-import com.firestar311.lib.command.Argument;
-
-public abstract class InputArgument extends Argument {
-}
--- Old Firestar311/FireLib/src/main/java/com/firestar311/lib/command/arg/SubCommand.java
+++
@@ -1,6 +0,0 @@
-package com.firestar311.lib.command.arg;
-
-import com.firestar311.lib.command.Argument;
-
-public class SubCommand extends Argument {
-}
