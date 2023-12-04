bcfce2739eda878c1a96b7ff25635a925b70668f
Firestar311 <slaterandrew0398@gmail.com>
2023-12-04 09:30:56 -0600

    Delete Old FirecraftMC/StarLib/src/main/java/com/starmediadev/lib/exceptions directory

--- Old FirecraftMC/StarLib/src/main/java/com/starmediadev/lib/exceptions/CornersNotInSameWorldException.java
+++
@@ -1,7 +0,0 @@
-package com.starmediadev.lib.exceptions;
-
-public class CornersNotInSameWorldException extends RuntimeException {
-    public CornersNotInSameWorldException() {
-        super("Corners are not in the same world.");
-    }
-}
